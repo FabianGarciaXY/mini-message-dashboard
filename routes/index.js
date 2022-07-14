7aaba8cf89d6d554f9eb7ff81809af8b584c1007
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-07-13 22:32:53 -0500

    Added routes for index and form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Mock Data
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+// GET home page
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Message-board', messages: messages });
 });
+
+
+router.get('/new', (req, res) => {
+  res.render('form', {title: 'New Message'});
+});
+
+
+router.post('/new', (req, res, next) => {
+  const user = req.body.user;
+  const message = req.body.message;
+  messages.push({text: message, user: user, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
